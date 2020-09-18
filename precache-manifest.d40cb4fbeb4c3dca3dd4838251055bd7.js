777a38f22c1ca1afdba39c816349679e360a0bb7
viprash <vishakhanaslapur@gmail.com>
2020-09-18 17:21:53 +0530

    Updates

---
+++ precache-manifest.d40cb4fbeb4c3dca3dd4838251055bd7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1392b35319abc1b5eb4333f8622c1105",
+    "url": "/content-listing-page/index.html"
+  },
+  {
+    "revision": "7e1b9ed96c4329a9ea15",
+    "url": "/content-listing-page/static/css/main.42da7fd3.chunk.css"
+  },
+  {
+    "revision": "df06419f06a1a6364bff",
+    "url": "/content-listing-page/static/js/2.c6a59854.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/content-listing-page/static/js/2.c6a59854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1b9ed96c4329a9ea15",
+    "url": "/content-listing-page/static/js/main.2b0faec9.chunk.js"
+  },
+  {
+    "revision": "c920b3694d42edcae990",
+    "url": "/content-listing-page/static/js/runtime-main.0fdc5e21.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/content-listing-page/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/content-listing-page/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/content-listing-page/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/content-listing-page/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/content-listing-page/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/content-listing-page/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/content-listing-page/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/content-listing-page/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/content-listing-page/static/media/poster9.65d6d500.jpg"
+  }
+]);
